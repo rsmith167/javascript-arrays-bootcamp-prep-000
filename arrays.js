777d47c171a220564a1a05a6b65f8e45d32bcb5d
Rwanda S <rwanda.ace@gmail.com>
2020-01-22 20:02:58 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(existingArray, newElement){
+  return existingArray.unshift(newElement)
+}
