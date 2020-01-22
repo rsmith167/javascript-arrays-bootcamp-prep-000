8be3db82488bdc1b1f99fc44100b97a818f0de7c
Rwanda S <rwanda.ace@gmail.com>
2020-01-22 20:04:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(existingArray, newElement){
   return existingArray.unshift(newElement)
 }
+function destructivelyAddElementToBeginningOfArray(existingArray, newElement){
+   existingArray = [newElement, ...existingArray]
+   return existingArray
+}
